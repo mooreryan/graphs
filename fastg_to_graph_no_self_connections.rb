364b0e24813036fff9a433a9f00742784d8e9afc
Ryan Moore <moorer@udel.edu>
2015-09-08 14:32:53 -0400

    Add script

---
+++ fastg_to_graph_no_self_connections.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+require 'parse_fasta'
+require 'fail_fast'
+include FailFast::Assertions
+
+FastaFile.open(ARGV.first).each_record do |head, seq|
+  # tildes mean something a bit more complicated in fastG
+  abort "\n\nOH SNAP A TILDE\n\n" if head.match /~/
+
+  # remove any of the reverse compliment headers -> ie treat rev comp
+  # the same as regulars
+  names = head.split(/[:;,]/).map { |head| head.sub(/'$/, '') }.uniq
+
+  if names.count > 1
+    names.each_cons(2).each do |arr|
+      puts arr.join "\t"
+    end
+  end
+end
